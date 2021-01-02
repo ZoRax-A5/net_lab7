0cbda2a4d9e0e95e10d61b8d8691fd108ce88cb5
zorax <707120797@qq.com>
2021-01-02 17:18:55 +0800

    server v1.0

--- server.cpp
+++ server.cpp
@@ -1,4 +1,10 @@
-// g++ server.cpp -o server -lwsock32 -lws2_32
+/*
+* server.cpp
+* name: Zong Weixu
+* id:   3180102776
+* compile: g++ server.cpp -o server -lwsock32 -lws2_32
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -16,7 +22,7 @@
 #pragma comment(lib, "Ws2_32")
 
 DWORD WINAPI connection(LPVOID ipParameter);
-void removeClient(sockaddr_in client_addr);
+sockaddr_in removeClient(SOCKET client_addr);
 
 struct ClientSocket
 {
@@ -51,6 +57,8 @@ public:
 
 Server::Server()
 {
+    // we can read server's information from config.ini in the future...
+    // I don't think it's necessary, though...
 }
 
 Server::~Server()
@@ -62,7 +70,6 @@ Server::~Server()
 
 void Server::start()
 {
-
     WSADATA wsaData;
     server_socket = INVALID_SOCKET;
     struct addrinfo *result = NULL,
@@ -128,8 +135,6 @@ void Server::start()
         throw "listen failed.\n";
     }
 
-    DWORD thread;
-
     while (true)
     {
         SOCKET client_socket;
@@ -151,7 +156,7 @@ void Server::start()
         // insert client socket in socket message queue
         clients_queue.push_back(ClientSocket(client_socket, client_addr));
         // create a thread for client and listen for its message
-        HANDLE hThread = CreateThread(NULL, 0, connection, &client_socket, 0, &thread);
+        HANDLE hThread = CreateThread(NULL, 0, connection, &client_socket, 0, NULL);
         if (hThread == NULL)
         {
             closesocket(server_socket);
@@ -168,47 +173,47 @@ DWORD WINAPI connection(LPVOID ipParameter)
     char read_buffer[BUFFER_LENGTH];
     char write_buffer[BUFFER_LENGTH];
     int iResult;
+    // optional
+    {
+        // send hello
+        const char *greet = "hello! Connection established successfully.\n";
+        send(socket, greet, strlen(greet), 0);
+    }
+    // keep on listening client's request
     while (true)
     {
+        // init read/write buffer
         memset(read_buffer, 0, sizeof(read_buffer));
         memset(write_buffer, 0, sizeof(write_buffer));
+
+        // receive package from client
         iResult = recv(socket, read_buffer, BUFFER_LENGTH, 0);
-        printf("ffff");
-        if (iResult < 0)
+
+        // socket closed by client forcibly
+        if (iResult <= 0)
         {
-            printf("recv failed: %d\n", WSAGetLastError());
-        }
-        else if (iResult == 0)
-        {
-            printf("Connection has been closed\n");
+            sockaddr_in del_addr = removeClient(socket);
+            printf("Client %s:%d disconnected.\n", inet_ntoa(del_addr.sin_addr), del_addr.sin_port);
             closesocket(socket);
-            //
             break;
         }
+        // deal&send package to client
         else
         {
             std::string s, ip_addr, port, message;
             SOCKET target = INVALID_SOCKET;
-            char type = read_buffer[0];
+            sockaddr_in del_addr;
+            // get request type of package
+            unsigned char type = read_buffer[0];
             switch (type)
             {
             case EXIT:
-                char *del_caddr;
-                int del_port;
-                for (auto iter = clients_queue.begin(); iter != clients_queue.end(); ++iter)
-                {
-                    if (iter->client_socket == socket)
-                    {
-                        del_caddr = inet_ntoa(iter->client_addr.sin_addr);
-                        del_port = iter->client_addr.sin_port;
-                        iter = clients_queue.erase(iter);
-                        break;
-                    }
-                }
-                printf("Client %s:%d disconnected.", del_caddr, del_port);
+                del_addr = removeClient(socket);
+                printf("Client %s:%d disconnected.\n", inet_ntoa(del_addr.sin_addr), del_addr.sin_port);
                 closesocket(socket);
                 return 0;
             case GET_TIME:
+                // [1 byte type] [time]
                 write_buffer[0] = SEND_TIME;
                 time_t t;
                 struct tm *lt;
@@ -218,11 +223,13 @@ DWORD WINAPI connection(LPVOID ipParameter)
                 send(socket, write_buffer, strlen(write_buffer) /*BUFFER_LEN*/, 0);
                 break;
             case GET_SERVER_NAME:
+                // [1 byte type] [server name]
                 write_buffer[0] = SEND_SERVER_NAME;
                 strcpy(write_buffer + strlen(write_buffer), server_name);
                 send(socket, write_buffer, strlen(write_buffer), 0);
                 break;
             case GET_CLIENT_LIST:
+                // [1 byte type] [ip#port$]*
                 write_buffer[0] = SEND_CLIENT_LIST;
                 for (auto iter = clients_queue.begin(); iter != clients_queue.end(); ++iter)
                 {
@@ -232,13 +239,14 @@ DWORD WINAPI connection(LPVOID ipParameter)
                 send(socket, write_buffer, strlen(write_buffer), 0);
                 break;
             case GET_CLIENT_MESSAGE:
-                // 1 byte type.ip#port($type(io))#message
+                // [1 byte type] [ip#port$] (type(io)) [message]
                 write_buffer[0] = SEND_CLIENT_MESSAGE;
                 s = std::string(write_buffer + 1);
                 ip_addr = s.substr(0, s.find("#"));
                 port = s.substr(s.find("#") + 1, s.find("$") - s.find("#") - 1);
                 message = s.substr(s.find("$") + 1);
 
+                // find target socket from client list
                 for (auto iter = clients_queue.begin(); iter != clients_queue.end(); ++iter)
                 {
                     char *caddr = inet_ntoa(iter->client_addr.sin_addr);
@@ -260,6 +268,7 @@ DWORD WINAPI connection(LPVOID ipParameter)
                 }
                 break;
             default:
+                // [unrecognizable type]
                 printf("failed to parses the request type of the packet.\n");
                 break;
             }
@@ -268,6 +277,20 @@ DWORD WINAPI connection(LPVOID ipParameter)
     return 0;
 }
 
+sockaddr_in removeClient(SOCKET socket)
+{
+    sockaddr_in addr;
+    for (auto iter = clients_queue.begin(); iter != clients_queue.end(); ++iter)
+    {
+        if (iter->client_socket == socket)
+        {
+            addr = iter->client_addr;
+            iter = clients_queue.erase(iter);
+            break;
+        }
+    }
+    return addr;
+}
 int main(int argc, char **argv)
 {
     try
